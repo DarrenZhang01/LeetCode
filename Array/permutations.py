ef3ff372758d4a663d1f2b002b197a358014f9cd
Darren <18633059886@163.com>
2018-06-13 20:13:00 -0400

    remove class

--- Array/permutations.py
+++ Array/permutations.py
@@ -1,24 +1,23 @@
 # Find permutations of a given List
-class Solution:
-    def permute(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        if (len(nums) == 0):
-            return [[]]
-        if (len(nums) == 1):
-            return [nums]
-        result = []
-        i = 0
-        while i < len(nums):
-            temp = []
-            if (i == 0):
-                temp = self.permute(nums[1:])
-            elif (i == len(nums) - 1):
-                temp = self.permute(nums[:len(nums)-1])
-            else:
-                temp = self.permute(nums[:i] + nums[i+1:])
-            result.extend([[nums[i]] + element for element in temp])
-            i += 1
-        return result
+def permute(nums):
+    """
+    :type nums: List[int]
+    :rtype: List[List[int]]
+    """
+    if (len(nums) == 0):
+        return [[]]
+    if (len(nums) == 1):
+        return [nums]
+    result = []
+    i = 0
+    while i < len(nums):
+        temp = []
+        if (i == 0):
+            temp = permute(nums[1:])
+        elif (i == len(nums) - 1):
+            temp = permute(nums[:len(nums)-1])
+        else:
+            temp = permute(nums[:i] + nums[i+1:])
+        result.extend([[nums[i]] + element for element in temp])
+        i += 1
+    return result
